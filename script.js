d2ebb0248d05b2a963d5c71762757d981412706c
MeseretHaile <ma4m21@gmail.com>
2026-02-26 23:45:51 -0600

    add the valid form submit

--- script.js
+++ script.js
@@ -46,7 +46,9 @@ function validCheck(e){
     let eMessage = document.getElementsByClassName("eMessage");
     let comment = document.getElementById("comment");
     let isValid = true;
-    
+    eMessage[0].classList.remove("error");
+    eMessage[3].classList.remove("error");
+
     if(fName.value.trim().length <= 1){
         eMessage[0].classList.add("error");
         isValid = false;
@@ -58,29 +60,51 @@ function validCheck(e){
     }
     
     let emailInput = document.getElementById("email");
-    let PhoneNumber = document.getElementById("PhoneNumber");
+    let phoneNumber = document.getElementById("phoneNumber");
     let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
 
-    emailInput.classList.remove("error");
-    let PhoneNumberReg = /^\d{5}$/;
+    eMessage[1].classList.remove("error");
+    eMessage[2].classList.remove("error");
+    let PhoneNumberReg = /^\d{9}$/;
 
     let emailRadio = document.getElementById("p-email");
     let phoneRadio = document.getElementById("p-phone");
 
-    if(emailRadio.checked && emailInput.value.trim() === "" || !emailReg.test(emailInput.value.trim())){
-        eMessage[2].classList.add("error");
-        isValid = false;
+    if(emailRadio.checked){
+        if(emailInput.value.trim() === "" || !emailReg.test(emailInput.value.trim())){
+            eMessage[2].classList.add("error");
+            isValid = false;
+        }
+    } 
+    else if(phoneRadio.checked){
+        if(phoneNumber.value.trim() === "" || !PhoneNumberReg.test(phoneNumber.value.trim())){
+            eMessage[1].classList.add("error");
+            isValid = false;
+        }
+    }
+
+    if(isValid){
+        let customer = {
+            fullName : fName.value.trim(),
+            phone: phoneNumber.value.trim(),
+            email: emailInput.value.trim(),
+            comments: comment.value.trim(),
+            preferredContact: emailRadio.checked ? "Email" : "Phone"
+        };
+
+        document.querySelector("#success").classList.remove("hide");
+        document.querySelector("#success").classList.add("show");
+
+        document.getElementById("formSub").innerHTML = `<strong>User Name: </strong>${customer.fullName}<br><strong>Preferred contact: </strong>${customer.preferredContact}<br><strong>Contact Information: </strong>${emailRadio.checked ? customer.email : customer.phone}<br><strong>Comment: </strong>${customer.comments}`;
+
+        document.getElementsByClassName("quoteForm").reset();
     }
 }   
    
 
 
 //email input from form 
-let emailInput = document.getElementById("email");
-let PhoneNumber = document.getElementById("PhoneNumber");
 
-let emailRe = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
-let form = document.getElementsByClassName("quoteForm");
 
 
 
